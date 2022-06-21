9b1d12edc5f37af0bdb356e1ee5f05a7b9df88b9
Denis <Kds04042002omgu@gmail.com>
2022-06-22 01:02:03 +0600

    Добавьте файлы проекта.

---
+++ CandF/Convert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandF
+{
+    internal class Convert
+    {
+        public static double CtoFandFtoC(double t, string type)
+        {
+            if (type.Equals("F") || type.Equals("f"))
+                return (t - 32) * 5 / 9;
+            else if (type.Equals("C") || type.Equals("c"))
+                return (t * 9) / 5 + 32;
+            return 404;
+        }
+    }
+}
---
+++ CandF/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите значение температуры: ");
+        string input = Console.ReadLine();
+        double t = Double.Parse(input);
+        do
+        {
+            Console.WriteLine("Введите тип шкалы: ");
+            input = Console.ReadLine();
+        } while (!input.Equals("F") && !input.Equals("f") && !input.Equals("C") && !input.Equals("c"));
+        Console.WriteLine("Результат: " + Math.Round(CandF.Convert.CtoFandFtoC(t, input), 2));
+    }
+}
